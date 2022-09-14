b093621bedbcfc5eb92dd57e6284633db974efcc
shinoshinohara <vshinohara@outlook.com>
2022-09-14 07:36:49 -0400

    : migrations
    tabela de usuario

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: '2022',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'test',
+    define: {
+        timestamps: true,
+        underscored:true,
+    }
+};
